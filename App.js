ab8f22e55fe6610382a7411bfabf6219d32efe9f
hamedkrishan <hamedkrishan676@gmail.com>
2025-01-21 21:32:36 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,51 @@
+import Task1 from "./componet/task1";
+import Task2 from "./componet/task2";
+import Task3 from "./componet/task3";
+import Task4 from "./componet/task4";
+import Task5 from "./componet/task5";
+import Task6 from "./componet/task6";
+import Task7 from "./componet/task7";
+import Task8 from "./componet/task8";
+import Task9 from "./componet/task9";
+import Task10 from "./componet/task10";
+import Task11 from "./componet/task11";
+import Task12 from "./componet/task12";
+import Task13 from "./componet/task13";
+import Task14 from "./componet/task14";
+
+
+function App() {
+  return (
+    <div classname="App">
+      <h1>React tasks</h1>
+      <Task1 />
+      <hr />
+      <Task2 />
+      <hr />
+      <Task3 />
+      <hr />
+      <Task4 />
+      <hr />
+      <Task5 />
+      <hr />
+      <Task6 />
+      <hr />
+      <Task7 />
+      <hr />
+      <Task8 />
+      <hr />
+      <Task9 />
+      <hr />
+      <Task10 />
+      <hr />
+      <Task11 />
+      <hr />
+      <Task12 />
+      <hr />
+      <Task13 />
+      <hr />
+      <Task14 />
+    </div>
+  );
+}
+export default App;
